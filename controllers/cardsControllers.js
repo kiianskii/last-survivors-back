3226f20494df19f93555d444f237b0dc3ac20aeb
kiianskii <ankiyanskiy@gmail.com>
2024-07-13 16:14:51 +0300

    upd delete

--- controllers/cardsControllers.js
+++ controllers/cardsControllers.js
@@ -42,10 +42,8 @@ export const getOneCard = async (req, res, next) => {
 export const deleteCard = async (req, res, next) => {
   try {
     const { id } = req.params;
-    const { board_id, column_id } = req.body;
+    // const { board_id, column_id } = req.body;
     const filter = {
-      board_id,
-      column_id,
       _id: id,
     };
     const result = await cardService.removeCard(filter);
