21284741b3bc477a168176d3be5f4289a24769b0
kiianskii <ankiyanskiy@gmail.com>
2024-07-17 11:03:07 +0300

    added authent

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -3,8 +3,11 @@ import validateBody from "../helpers/validateBody.js";
 import isEmptyBody from "../middlewares/emptyBodyCheck.js";
 import { updateUserSchema } from "../schemas/userSchemas.js";
 import userControllers from "../controllers/userControllers.js";
+import authenticate from "../middlewares/authenticate.js";
 
 const userRouter = express.Router();
+
+userRouter.use(authenticate);
 
 userRouter.patch(
   "/:id",
